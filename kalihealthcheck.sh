13b93a8a9df1d622a8f68cb08dff1bfcdc6661f8
Ewerson Guimaraes (Crash) <crash@dclabs.com.br>
2017-04-07 16:45:24 +0200

    Added Deep Check function
    Added Deep Check function

--- kalihealthcheck.sh
+++ kalihealthcheck.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-export logfile='/tmp/kalicheck.txt'
+export logfile=kalicheck.txt
 export update=('apt update && apt upgrade' 'apt-get update && apt-get dist-upgrade')
 export repos=('/etc/apt/sources.list')
 export docs=('http://docs.kali.org/general-use/kali-linux-sources-list-repositories')
 export kernel=("$(uname -v | awk '{print$4}')" "curl -sL http://pkg.kali.org/linux | awk '/version:/ {getline;print$1}'")
+export userhashlist=userhashlist.txt
+export dirlist=dislist.txt
+export filelist=filelist.txt
+export hc=/usr/bin/sha1sum
+export originalhashlist=originalhashlist.txt
 clear
+rm -f  originalhashlist 2> /dev/null
 printf '\e[4m\e[1m%s\n\e[0m' "Kali Health Check"
 
 printf '%s\n' "[-] Checking sources.list file..." | tee $logfile
@@ -13,22 +19,19 @@ printf '%s\n' "[-] Checking sources.list file..." | tee $logfile
 if [[ -s "$repos" ]] && [[ "$(grep -v '#' /etc/apt/sources.list | sed '/^$/d' | wc -l)" != 1 ]] ;
 then
 	printf '\e[91m\e[1m%s\n\e[0m' "Non-standard repository configuration detected. - [FAIL]" | tee -a $logfile
-	
 	printf '%s\n' "Please check this documentation: "$repos"."
-	
 	printf '%s\n' "Any additional repositories added to Kali's sources.list file will most likely BREAK YOUR INSTALL."
-	
 	printf '%s\n' "++++++++++SOURCES++++++++++" >> $logfile
 	cat               /etc/apt/sources.list     >> $logfile
 	printf '%s\n' "+++++++++++++++++++++++++++" >> $logfile
 else
 	printf '%s\n' "[-] Checking repository URL..."
-	
+
 	if grep -q "deb http://http.kali.org/kali kali-rolling main contrib non-free" <<< "$(grep -v '#' /etc/apt/sources.list | sed '/^$/d')" ||
 	   grep -q "deb http://http.kali.org/kali kali-rolling main non-free contrib" <<< "$(grep -v '#' /etc/apt/sources.list | sed '/^$/d')" ;
 	then
 		printf '\e[92m\e[1m%s\n\e[0m' "Standard repository configuration detected. - [PASS]" | tee -a $logfile
-		
+
 	else
 		printf '\e[91m\e[1m%s\n\e[0m' "Non-standard repository configuration detected. - [FAIL]" | tee -a $logfile
 		printf '%s\n' "Please check this documentation: http://docs.kali.org/general-use/kali-linux-sources-list-repositories."
@@ -38,7 +41,7 @@ else
 		printf '%s\n' "+++++++++++++++++++++++++++" >> $logfile
 
 		printf '%s\n' "Do you want to restore your repository configuration's default settings? [y/n]."
-		
+
 		read -n 1 -r -s restore
 		case "$restore" in
 			y)
@@ -62,14 +65,12 @@ else
 fi
 
 printf '%s\n' "[-] Checking kernel..." | tee -a $logfile
-
+sleep 1
 if grep -q "$(curl -s http://pkg.kali.org/pkg/linux | grep -A 1 version: | sed '1d' | sed 's/ //g')" <<< "$(uname -v)" ;
 then
 	printf '\e[92m%s\n\e[0m' "Latest kernel detected. - [PASS]" | tee -a $logfile
-	
 else
 	printf '\e[91m\e[1m%s\n\e[0m' "Latest kernel not detected. - [FAIL]" | tee -a $logfile
-	5
 printf 'Consider running \e[1m%s\e[0m to get up to date.\n' "$update"
 	printf '%s\n' "++++++++++KERNEL+++++++++++" >> $logfile
 	uname -a                                    >> $logfile
@@ -91,7 +92,64 @@ else
         printf '%s\n' "+++++++++++++++++++++++++++"      >> $logfile
 fi
 
-printf '%s\n' "A log file has been generated." "Would you like to share it online? [y/n]."
+printf '\e[91m\e[1m%s\n\e[0m' "Would you like to perform a deep checking ***EXPERIMENTAL and TAKES A LONG TIME - 64bit only [y/N]." 
+read -n 1 -r -s choice
+if [[ "$choice" != *[yY]* ]] ;
+then
+	printf '%s\n' "Deep checking skipped" | tee -a $logfile
+else
+/usr/bin/apt-file update
+echo "/usr/share/metasploit-framework/lib/snmp" > ${dirlist}
+echo > "${filelist}"
+echo -n > "${userhashlist}"
+while IFS= read -r line
+do
+	find "${line}" -iname "*" -type f | egrep -iv "(\.png$|\.jpg$|\.gif$|\.jpeg$|\.ico$|\.svg$|\.log$|\.gz$|\.tar$|\.page$|\.css$|\.zip$|\.mo$|exploitdb|password|shadow)" > "${filelist}" #retrives file list
+	while IFS= read -r line2
+	do
+		echo "$("${hc}" "${line2}")" | tee -a "${userhashlist}" 
+	done < "${filelist}" 
+    
+done < "${dirlist}"
+#### Download original hash list ####
+#####################################
+echo " Downloading Original Hask List"
+wget https://github.com/crashbrz/kalihealthcheck/raw/master/originalfilehash.txt.tar.gz
+tar -xzvf  originalfilehash.txt.tar.gz
+mv originalfilehash.txt originalhashlist.txt
+####################################
+while IFS= read -r linecheck #While read the old list of files.
+do
+	originalfilepath=$(echo "${linecheck}" | rev | cut -d / -f 2-99999 | rev | cut -d " " -f 3-99999)
+	originalfilename=$(echo "${linecheck}" | rev | cut -d / -f 1 | rev)
+	originalfilehash=$(echo "${linecheck}" | awk '{print $1}')
+	mergefilepath="${originalfilepath}/${originalfilename}"
+	userfilepath=$(cat ${userhashlist} | grep "${mergefilepath}$" | rev | cut -d / -f 2-99999 | rev)
+	userfilename=$(cat ${userhashlist} | grep "${mergefilepath}$" | rev | cut -d / -f 1 | rev)
+	userfilehash=$(cat ${userhashlist} | grep "${mergefilepath}$" | awk '{print $1}')
+      	
+	if test "${userfilehash}" != "${originalfilehash}" ; then
+		echo "===============================================================" | tee -a $logfile
+		echo "The file: "${mergefilepath}"" | tee -a $logfile
+		echo "doesn't match with the original Kali Linux last version file" | tee -a $logfile
+		echo "===============================================================" | tee -a $logfile
+		echo "In order to solve this issue, please remove and reinstall the package:" | tee -a $logfile
+		pkgname=$(/usr/bin/apt-file find ${mergefilepath} | cut -d : -f1)  
+		echo "${pkgname}"  | tee -a $logfile
+		/usr/bin/apt-get download "${pkgname}"
+		dpkg -x *.deb .
+		echo "Following  the currupted information:" | tee -a $logfile
+		diff -C0 "${mergefilepath}" "./${mergefilepath}"  | tee -a $logfile
+		else
+	
+		echo "OK - ${mergefilepath}"
+	fi
+
+done < "${originalhashlist}"
+
+fi
+
+printf '%s\n' "A log file has been generated." "Would you like to share it online? [Y/n]."
 read -n 1 -r -s choice
 if [[ "$choice" != *[nN]* ]] ;
 then
