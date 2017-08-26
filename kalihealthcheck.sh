7732392498e7627816432b068516bf357456100d
Ewerson Guimaraes (Crash) <crash@dclabs.com.br>
2017-08-26 09:46:36 +0200

    Update
    Some logs added

--- kalihealthcheck.sh
+++ kalihealthcheck.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export logfile=kalicheck.log
+export logfile='/tmp/kalicheck.txt'
 export update=('apt update && apt upgrade' 'apt-get update && apt-get dist-upgrade')
 export repos=('/etc/apt/sources.list')
 export docs=('http://docs.kali.org/general-use/kali-linux-sources-list-repositories')
@@ -12,6 +12,7 @@ export hc=/usr/bin/sha1sum
 export originalhashlist=originalhashlist.txt
 clear
 rm -f  originalhashlist 2> /dev/null
+
 printf '\e[4m\e[1m%s\n\e[0m' "Kali Health Check"
 
 printf '%s\n' "[-] Checking sources.list file..." | tee $logfile
@@ -65,7 +66,7 @@ else
 fi
 
 printf '%s\n' "[-] Checking kernel..." | tee -a $logfile
-
+sleep 1
 if grep -q "$(curl -s http://pkg.kali.org/pkg/linux | grep -A 1 version: | sed '1d' | sed 's/ //g')" <<< "$(uname -v)" ;
 then
 	printf '\e[92m%s\n\e[0m' "Latest kernel detected. - [PASS]" | tee -a $logfile
@@ -91,25 +92,44 @@ else
         tail -1 /var/log/apt/history.log | cut -b 11-21  >> $logfile
         printf '%s\n' "+++++++++++++++++++++++++++"      >> $logfile
 fi
+#Thanks muts for the suggestion 
+	printf '%s\n' "[-] Collecting system logs..." | tee -a $logfile
+	printf '\e[91m\e[1m%s\n\e[0m' "Please, consider to clear the logs(/var/log/messages /var/log/kern.log /var/log/syslog /var/log/user.log), reproduce the bug and run this script again to better locate the issue." | tee -a $logfile
+	sleep 1
+	printf '%s\n' "++++++++++LOG: MESSAGES+++++++++++" >> $logfile
+	cat /var/log/messages >> $logfile
+	printf '%s\n' "[-] MESSAGES - [DONE"] | tee -a $logfile
+	printf '%s\n' "+++++++++++++++++++++++++++" >> $logfile
+	printf '%s\n' "++++++++++LOG: KERN.LOG+++++++++++" >> $logfile
+	cat /var/log/kern.log >> $logfile
+	printf '%s\n' "[-] KERN.LOG - [DONE"] | tee -a $logfile
+	printf '%s\n' "+++++++++++++++++++++++++++" >> $logfile
+	printf '%s\n' "++++++++++LOG: SYSLOG+++++++++++" >> $logfile
+	cat /var/log/syslog >> $logfile
+	printf '%s\n' "[-] SYSLOG - [DONE"] | tee -a $logfile
+	printf '%s\n' "+++++++++++++++++++++++++++" >> $logfile
+	printf '%s\n' "++++++++++LOG: USER.LOG+++++++++++" >> $logfile
+	cat /var/log/user.log >> $logfile
+	printf '%s\n' "[-] USERLOG - [DONE"] | tee -a $logfile
+	printf '%s\n' "+++++++++++++++++++++++++++" >> $logfile
+	printf '%s\n' "[-] Collecting system info..." | tee -a $logfile
+	printf '%s\n' "++++++++++DMESG+++++++++++" >> $logfile
+	dmesg >> $logfile
+	printf '%s\n' "[-] DMESG - [DONE"] | tee -a $logfile
+	printf '%s\n' "+++++++++++++++++++++++++++" >> $logfile
 
-printf '%s\n' "[-] Fetching Mirror list..." | tee -a $logfile
-curl -sI http://http.kali.org/README  >> $logfile
-curl -sI http://http.kali.org/README  | grep -i  "MirrorBrain" 
-printf '%s\n' "+++++++++++++++++++++++++++" | tee -a $logfile
-
-printf '\e[91m\e[1m%s\n\e[0m' "Would you like to perform a deep checking ***EXPERIMENTAL and TAKES A LONG TIME - 64bit only [y/N]." 
+printf '\e[91m\e[1m%s\n\e[0m' "Would you like to perform a deep checking ***EXPERIMENTAL and TAKES A LONG TIMEs*** [y/N]."
 read -n 1 -r -s choice
 if [[ "$choice" != *[yY]* ]] ;
 then
-	printf '%s\n' "Deep checking skipped" | tee -a $logfile
+	printf 'Deep checking skipped \e[1m'
 else
-/usr/bin/apt-file update
 echo "/usr/share/metasploit-framework/lib/snmp" > ${dirlist}
 echo > "${filelist}"
-echo -n > "${userhashlist}"
+echo > "${userhashlist}"
 while IFS= read -r line
 do
-	find "${line}" -iname "*" -type f | egrep -iv "(\.png$|\.jpg$|\.gif$|\.jpeg$|\.ico$|\.svg$|\.log$|\.gz$|\.tar$|\.page$|\.css$|\.zip$|\.mo$|exploitdb|password|shadow)" > "${filelist}" #retrives file list
+	find "${line}" -iname "*" -type f | egrep -iv "(\.png|\.jpg|\.gif|\.jpeg|\.ico|\.svg|\.log|\.gz|\.tar|\.page|\.css|\.zip|exploitdb|password|shadow|android)" > "${filelist}" #retrives file list
 	while IFS= read -r line2
 	do
 		echo "$("${hc}" "${line2}")" | tee -a "${userhashlist}" 
@@ -118,32 +138,38 @@ do
 done < "${dirlist}"
 #### Download original hash list ####
 #####################################
-echo " Downloading Original Hask List"
-wget https://github.com/crashbrz/kalihealthcheck/raw/master/originalfilehash.txt.tar.gz
-tar -xzvf  originalfilehash.txt.tar.gz
-mv originalfilehash.txt originalhashlist.txt
+
+
 ####################################
 while IFS= read -r linecheck #While read the old list of files.
 do
 	originalfilepath=$(echo "${linecheck}" | rev | cut -d / -f 2-99999 | rev | cut -d " " -f 3-99999)
 	originalfilename=$(echo "${linecheck}" | rev | cut -d / -f 1 | rev)
 	originalfilehash=$(echo "${linecheck}" | awk '{print $1}')
+	
 	mergefilepath="${originalfilepath}/${originalfilename}"
+
 	userfilepath=$(cat ${userhashlist} | grep "${mergefilepath}$" | rev | cut -d / -f 2-99999 | rev)
 	userfilename=$(cat ${userhashlist} | grep "${mergefilepath}$" | rev | cut -d / -f 1 | rev)
 	userfilehash=$(cat ${userhashlist} | grep "${mergefilepath}$" | awk '{print $1}')
-      	
+        #echo "==============================================================================" 
+		#echo "Conteudo userfilehash     $userfilehash"
+		#echo "Conteudo originalfilehash $originalfilehash"
+        #echo "==============================================================================" 
+	
 	if test "${userfilehash}" != "${originalfilehash}" ; then
 		echo "===============================================================" | tee -a $logfile
-		echo "The file: "${mergefilepath}"" | tee -a $logfile
+		echo "The file: ("${mergefilepath}")" | tee -a $logfile
 		echo "doesn't match with the original Kali Linux last version file" | tee -a $logfile
 		echo "===============================================================" | tee -a $logfile
 		echo "In order to solve this issue, please remove and reinstall the package:" | tee -a $logfile
+		apt-file updade
 		pkgname=$(/usr/bin/apt-file find ${mergefilepath} | cut -d : -f1)  
 		echo "${pkgname}"  | tee -a $logfile
 		/usr/bin/apt-get download "${pkgname}"
 		dpkg -x *.deb .
-		echo "Following  the currupted information:" | tee -a $logfile
+		echo "Following  the curruption information:" 
+		# diff -C0 "${mergefilepath}" "./${mergefilepath}" | egrep -i "(^\*\*\*.*.\*\*\*\*$|^\-\-\-.*.\-\-\-\-$)"  | egrep [0-9] | sed 's/\*\*\* /The corrupted line from your file is\: /g' | sed 's/\*\*\*\*//g' | sed 's/--- /The original line is: /g' | sed 's/ ----//g'
 		diff -C0 "${mergefilepath}" "./${mergefilepath}"  | tee -a $logfile
 		else
 	
